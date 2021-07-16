e6044b94342fb0a858dd7cfc9239c73f8b5b099c
sunokk125 <sunokk125@naver.com>
2021-07-16 13:29:03 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Menu/>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin:0 auto; 
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="menu_wrapper">
+        <div class="menu">
+            메뉴1
+            <div>
+                <p>서브메뉴1</p>
+                <p>서브메뉴2</p>
+            </div>
+        </div>
+        <div class="menu">
+            메뉴1
+            <div>
+                <p>서브메뉴1</p>
+                <p>서브메뉴2</p>
+            </div>
+        </div>
+        <div class="menu">
+            메뉴1
+            <div>
+                <p>서브메뉴1</p>
+                <p>서브메뉴2</p>
+            </div>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .menu_wrapper{
+        width:100%;
+        display:flex;
+        justify-content: center;
+    }
+    .menu{
+        width:10%;
+    }
+    ul{
+        width:100%;
+        list-style:none;
+    }
+    li{
+        margin: 0 5%;
+        border: 1px solid #cccccc;
+        height: 50px;
+        text-align: center;
+        line-height: 50px;
+    }
+</style>
